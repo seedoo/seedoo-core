27d82dafbd6b1e1f94b049caf2da2dafbcb05b40
ortese <francesco.alba.82@gmail.com>
2017-07-06 18:26:43 +0200

    versioning

--- seedoo_dematerializzazione/__openerp__.py
+++ seedoo_dematerializzazione/__openerp__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Seedoo Dematerializzazione',
-    'version': '8.0.0.0.0',
+    'version': '8.0.1.0.0',
     'category': 'Document Management',
     'summary': 'Gestione Documenti Analogici Protocollo',
     'author': 'Flosslab',
