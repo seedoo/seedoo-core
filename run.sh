8414c2b0e175c9fed4c87c7aebeba7db878bec24
Francesco Alba <francesco.alba@flosslab.com>
2017-09-21 09:58:16 +0200

    Aggiunto run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+Xvfb :10 &
+
+export DISPLAY=":10"
+
+./venv/bin/python ./ocb/openerp-server \
+    --addons-path=ocb/openerp/addons,ocb/addons,core,addons/seedoo-attivita,oca/l10n-italy \
+    --xmlrpc-port=8069 \
+    --db_host=127.0.0.1 \
+    --db_port=5432 \
+    --db_user=seedoo \
+    --db_password=seedoo \
+    --logfile=$HOME/seedoo.log &
