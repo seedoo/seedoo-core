db24e88ab12ea728717d046138d1195e7aef7c7c
andrea.peruzzu <andrea.peruzzu@flosslab.com>
2018-02-01 17:45:34 +0100

    cambiato stile pulsanti form protocollo

--- seedoo_protocollo/static/src/js/protocollo_accordion.js
+++ seedoo_protocollo/static/src/js/protocollo_accordion.js
@@ -1,11 +1,21 @@
-    $(document).on('click', '#protocollo-accordion .protocollo-accordion-toggle', function() {
-         //Expand or collapse this panel
-      $("#protocollo-accordion .protocollo-accordion-toggle").removeClass("accordion-selected");
-      $(this).next().slideToggle('fast');
-      $(this).addClass("accordion-selected");
+$(document).on('click', '#protocollo-accordion .protocollo-accordion-toggle', function () {
 
-      //Hide the other panels
-      // $(".accordion-content").not($(this).next()).slideUp('fast');
+    //Close "Anteprima Panel" if is closing "Documento Panel"
+    if ($(this).parent().attr("id") == "documento-container") {
+        console.log("elem" + $(this).next().attr("class"))
+        if ($(this).next().css("display") == "block") {
+            $("#anteprima-container").css("display", "none")
+        }
+        else {
+            $("#anteprima-container").css("display", "block")
+        }
 
+    }
 
-    });
+    $("#protocollo-accordion .protocollo-accordion-toggle").removeClass("accordion-selected");
+    $(this).next().slideToggle('fast');
+    $(this).addClass("accordion-selected");
+    //Hide the other panels
+    // $(".accordion-content").not($(this).next()).slideUp('fast');
+});
+
