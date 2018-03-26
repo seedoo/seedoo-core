88854d1e5b698f92eea23e5b9e1aab0ce5dbf94d
andrea.peruzzu <andrea.peruzzu@flosslab.com>
2018-03-26 15:41:36 +0200

    completato wizard conferma registrazione con assegnatari e mitt/det

--- seedoo_protocollo/wizard/registration_protocollo_wizard.py
+++ seedoo_protocollo/wizard/registration_protocollo_wizard.py
@@ -8,12 +8,14 @@ class protocollo_registration_confirmation_wizard(models.TransientModel):
     _description = 'Wizard di Conferma Registrazione Protocollo'
 
     _columns = {
+        'senders': fields.html('Mittenti', readonly=True),
+        'receivers': fields.html('Destinatari', readonly=True),
         'subject': fields.char('Oggetto', readonly=True),
+        'assegnatario_competenza_id': fields.many2one('protocollo.assegnatario',
+                                                      'Assegnatario Competenza',
+                                                      required=True, readonly=True),
+        'assegnatario_conoscenza_ids': fields.html('Assegnatari CC', readonly=True),
         'message_verifica_campi_obbligatori': fields.html('Verifica campi obbligatori', readonly=True)
-        # 'assegnatari_competenza_uffici_ids': fields.one2many('protocollo.assegna.ufficio.wizard',
-        #          'competenza_uffici_wizard_id', 'Uffici Assegnatari per Competenza'),
-        # 'assegnatari_competenza_dipendenti_ids': fields.one2many('protocollo.assegna.dipendente.wizard',
-        #          'competenza_dipendenti_wizard_id', 'Dipendenti Assegnatari per Competenza'),
     }
 
     def _default_protocollo_subject(self, cr, uid, context):
@@ -22,6 +24,61 @@ class protocollo_registration_confirmation_wizard(models.TransientModel):
         else:
             return None
         return protocollo.subject
+
+    def _default_protocollo_senders(self, cr, uid, context):
+        if 'active_id' in context:
+            protocollo = self.pool.get('protocollo.protocollo').browse(cr, uid, context['active_id'])
+            if protocollo.senders:
+                senders_list = ''
+                for sender in protocollo.sender_receivers:
+                    senders_list = senders_list + sender.name + '</br>'
+                return senders_list
+        return False
+
+    def _default_protocollo_receivers(self, cr, uid, context):
+        if 'active_id' in context:
+            protocollo = self.pool.get('protocollo.protocollo').browse(cr, uid, context['active_id'])
+            if protocollo.receivers:
+                receivers_list = ''
+                for receiver in protocollo.sender_receivers:
+                    receivers_list = receivers_list + receiver.name + '</br>'
+                return receivers_list
+        return False
+
+    def _default_assegnatario_competenza_id(self, cr, uid, context):
+        if 'active_id' in context:
+            assegnazione_obj = self.pool.get('protocollo.assegnazione')
+            assegnazione_ids = assegnazione_obj.search(cr, uid, [
+                ('protocollo_id', '=', context['active_id']),
+                ('tipologia_assegnazione', '=', 'competenza'),
+                ('parent_id', '=', False)
+            ])
+            if assegnazione_ids:
+                assegnazione = assegnazione_obj.browse(cr, uid, assegnazione_ids[0])
+                return assegnazione.assegnatario_id.id
+        return False
+
+    def _default_assegnatario_conoscenza_ids(self, cr, uid, context):
+        if 'active_id' in context:
+            assegnazione_obj = self.pool.get('protocollo.assegnazione')
+            assegnazione_ids = assegnazione_obj.search(cr, uid, [
+                ('protocollo_id', '=', context['active_id']),
+                ('tipologia_assegnazione', '=', 'conoscenza'),
+                ('parent_id', '=', False)
+            ])
+            assegnatari_conoscenza_list = ''
+            if assegnazione_ids:
+                assegnazione_ids = assegnazione_obj.browse(cr, uid, assegnazione_ids)
+                for assegnazione in assegnazione_ids:
+                    if assegnazione.tipologia_assegnatario == 'department':
+                        assegnatari_conoscenza_list = assegnatari_conoscenza_list + \
+                                                        assegnazione.assegnatario_department_id.name + '</br>'
+                    else:
+                        assegnatari_conoscenza_list = assegnatari_conoscenza_list + \
+                                                        assegnazione.assegnatario_employee_department_id.name + ' / ' + \
+                                                        assegnazione.assegnatario_employee_id.name + '</br>'
+                return assegnatari_conoscenza_list
+        return False
 
     def _default_message_verifica_campi_obbligatori(self, cr, uid, context):
         if 'active_id' in context:
@@ -54,8 +111,12 @@ class protocollo_registration_confirmation_wizard(models.TransientModel):
     #     return assegnatari
 
     _defaults = {
+        'senders': _default_protocollo_senders,
+        'receivers': _default_protocollo_receivers,
         'subject': _default_protocollo_subject,
-        'message_verifica_campi_obbligatori': _default_message_verifica_campi_obbligatori
+        'message_verifica_campi_obbligatori': _default_message_verifica_campi_obbligatori,
+        'assegnatario_competenza_id': _default_assegnatario_competenza_id,
+        'assegnatario_conoscenza_ids': _default_assegnatario_conoscenza_ids
         # 'assegnatari_competenza_uffici_ids': _default_assegnatari_competenza_uffici_ids,
         # 'assegnatari_competenza_dipendenti_ids': _default_assegnatari_competenza_dipendenti_ids,
     }
