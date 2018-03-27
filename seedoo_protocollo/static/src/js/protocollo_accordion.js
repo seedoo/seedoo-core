daf059b60045bdd5a73c03addab74b4ab5a867fe
andrea.peruzzu <andrea.peruzzu@flosslab.com>
2018-03-27 12:09:52 +0200

    bug-fix accordion in form protocollo

--- seedoo_protocollo/static/src/js/protocollo_accordion.js
+++ seedoo_protocollo/static/src/js/protocollo_accordion.js
@@ -1,6 +1,6 @@
 $(document).on('click', '#protocollo-accordion .protocollo-accordion-toggle', function () {
 
-    //Close "Anteprima Panel" if is closing "Documento Panel"
+    // Close "Anteprima Panel" if is closing "Documento Panel"
     if ($(this).parent().attr("id") == "documento-container") {
         if ($(this).next().css("display") == "block") {
             $("#anteprima-container").css("display", "none")
@@ -8,12 +8,17 @@ $(document).on('click', '#protocollo-accordion .protocollo-accordion-toggle', fu
         else {
             $("#anteprima-container").css("display", "block")
         }
-
     }
 
-    $("#protocollo-accordion .protocollo-accordion-toggle").removeClass("accordion-selected");
+
     $(this).next().slideToggle('fast');
-    $(this).addClass("accordion-selected");
+
+    if($(this).hasClass("open")){
+        $(this).removeClass("open");
+    }
+    else {
+        $(this).addClass("open");
+    }
     //Hide the other panels
     // $(".accordion-content").not($(this).next()).slideUp('fast');
 });
