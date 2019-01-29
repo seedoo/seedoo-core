88b2f3a1a9bda02687e4501b73c4a64e0dfdbe6b
francesco.alba <francesco.alba@flosslab.com>
2019-01-29 17:42:56 +0100

    aggiornamento versione

--- seedoo_theme/__openerp__.py
+++ seedoo_theme/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Seedoo Theme",
-    "version": "8.0.1.8.0",
+    "version": "8.0.1.8.1",
     "category": "Web",
     "summary": "Tema Grafico Piattaforma Seedoo",
     "sequence": "1",
