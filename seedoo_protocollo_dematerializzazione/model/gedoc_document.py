ab0a69e1ae261b65c186cf8071c8b86cb09040eb
andrea.peruzzu <andrea.peruzzu@flosslab.com>
2019-03-06 14:34:44 +0100

    Riorganizzazione elementi in vista form protocollo

--- seedoo_protocollo_dematerializzazione/model/gedoc_document.py
+++ seedoo_protocollo_dematerializzazione/model/gedoc_document.py
@@ -96,7 +96,7 @@ class gedoc_document(osv.Model):
 
 
     _columns = {
-        'importer_id': fields.many2one('dematerializzazione.storico.importazione.importer', 'Importer', readonly=True),
+        'importer_id': fields.many2one('dematerializzazione.storico.importazione.importer', 'Importazione', readonly=True),
         'imported': fields.boolean('Importato', readonly=True),
         'protocollo_id': fields.many2one('protocollo.protocollo', 'Protocollo', readonly=True),
         'typology_id': fields.many2one('protocollo.typology', 'Tipologia', readonly=True),
