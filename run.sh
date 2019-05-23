bbe037d15e25a1679f7594d2922c75ac8d6e3435
Luca Cireddu <sardylan@gmail.com>
2019-05-23 18:13:43 +0200

    Fixes run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-Xvfb :10 &
-
-export DISPLAY=":10"
-
-./venv/bin/python ./ocb/openerp-server \
-    --addons-path=ocb/openerp/addons,ocb/addons,web,seedoo-core,l10n-italy,server-tools \
+./venv/bin/python ./OCB/openerp-server \
+    --addons-path=OCB/openerp/addons,OCB/addons,web,seedoo-core,l10n-italy,server-tools \
     --xmlrpc-port=8069 \
     --db_host=127.0.0.1 \
     --db_port=5432 \
     --db_user=seedoo \
     --db_password=seedoo \
     --data-dir=~/seedoo/data \
-    --logfile=$HOME/seedoo.log &
+    --logfile=~/seedoo/log/seedoo.log &
+    
