3696f9e731b77cd7716fd5c1d6a2f914c49073e8
Saul Lai <saul.lai@flosslab.com>
2019-08-01 09:38:21 +0200

    Parsing dell'header receiver

--- seedoo_protocollo/model/mail_thread.py
+++ seedoo_protocollo/model/mail_thread.py
@@ -4,6 +4,13 @@
 
 from openerp.osv import orm, fields, osv
 from openerp.addons.mail.mail_message import decode
+from openerp import api, tools
+import datetime
+import dateutil
+import logging
+import pytz
+
+_logger = logging.getLogger(__name__)
 
 def decode_header(message, header, separator=' '):
     return separator.join(map(decode, filter(None, message.get_all(header, []))))
@@ -22,3 +29,26 @@ class MailThread(orm.Model):
                 message._headers.append(delivered_to_header)
 
         return super(MailThread, self).message_route(cr, uid, message, message_dict, model, thread_id, custom_values, context)
+
+    def message_parse(self, cr, uid, message, save_original=False, context=None):
+        msg_dict = super(MailThread, self).message_parse(cr, uid, message, save_original, context)
+        if message.get('Received'):
+            try:
+                received_hdr = decode(message.get('Received'))
+                received_hdr_list = received_hdr.split(';')
+                date_hdr = received_hdr_list[1]
+                parsed_date = dateutil.parser.parse(date_hdr, fuzzy=True)
+                if parsed_date.utcoffset() is None:
+                    # naive datetime, so we arbitrarily decide to make it
+                    # UTC, there's no better choice. Should not happen,
+                    # as RFC2822 requires timezone offset in Date headers.
+                    stored_date = parsed_date.replace(tzinfo=pytz.utc)
+                else:
+                    stored_date = parsed_date.astimezone(tz=pytz.utc)
+            except Exception:
+                _logger.warning('Failed to parse Received header %r in incoming mail '
+                                'with message-id %r, assuming current date/time.',
+                                message.get('Received'), msg_dict['message_id'])
+                stored_date = datetime.datetime.now()
+            msg_dict['date_received'] = stored_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
+        return msg_dict
