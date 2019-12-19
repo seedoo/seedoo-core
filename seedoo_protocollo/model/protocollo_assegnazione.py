d00e53e00b1721e910f823c31c93f1ae73348322
andrea.peruzzu <andrea.peruzzu@flosslab.com>
2019-12-19 13:07:56 +0100

    Inserito nel template Assegnazione protocollo il link diretto alla pagina del protocollo

--- seedoo_protocollo/model/protocollo_assegnazione.py
+++ seedoo_protocollo/model/protocollo_assegnazione.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # This file is part of Seedoo.  The COPYRIGHT file at the top level of
 # this module contains the full copyright notices and license terms.
+import werkzeug
 
-from openerp import SUPERUSER_ID, tools
+from openerp import SUPERUSER_ID, tools, api
 from openerp.osv import orm, fields, osv
 from openerp.tools.translate import _
 from util.selection import *
@@ -676,3 +677,18 @@ class protocollo_assegnazione(orm.Model):
                     self.notifica_assegnazione(cr, uid, child)
         except Exception as e:
             _logger.error(e.message)
+
+    @api.multi
+    def get_full_url(self):
+        self.ensure_one()
+        base_url = self.env["ir.config_parameter"].get_param("web.base.url")
+        url_params = {
+            'id': self.protocollo_id.id,
+            'view_type': 'form',
+            'model': 'protocollo.protocollo',
+            'menu_id': self.env.ref('seedoo_protocollo.protocollo_protocollo_notified_menu').id,
+            'action': self.env.ref('seedoo_protocollo.protocollo_protocollo_act').id,
+        }
+
+        params = '/web?#%s' % werkzeug.url_encode(url_params)
+        return base_url + params
